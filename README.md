7c4e375abf28dda7279770b2f286aaf5f0c44956
Nevylish <yanisfaria710@gmail.com>
2020-07-24 17:30:11 +0200

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+# Modification du menu pause pour FiveM
+#### Ce script vous permet de modifier les différentes entrées principale du menu pause (Echap/P).
+
+Screenshot : (deleted)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+fx_version 'adamant'
+game 'gta5'
+
+description 'Menu personnalisable fait par Garfield'
+
+client_script 'pausemenu.lua'
